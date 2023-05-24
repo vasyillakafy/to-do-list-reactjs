06d2087f2d27ad85f7032b784fea1bf1e3b56d36
vasyillakafy <vasyillakafy@gmail.com>
2023-05-25 01:27:07 +0700

    added store

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter, Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { 
+  Route, 
+  RouterProvider, 
+  createBrowserRouter, 
+  createRoutesFromElements 
+} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { configureStore } from "@reduxjs/toolkit";
+
+import { loadFromLocalStorage, saveToLocalStorage } from './utils/localStorage';
+import todosReducer from "./features/todoSlice";
+
 import Layout from './components/Layout.jsx'
 import All from './pages/All.jsx'
 import Active from './pages/Active.jsx'
 import Completed from './pages/Completed.jsx'
+import ErrorPage from './pages/ErrorPage.jsx'
+
+const store =  configureStore({
+  reducer: {
+    todos: todosReducer,
+  },
+  preloadedState: loadFromLocalStorage()
+});
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route>
-        <Route path='/' element={<Layout/>}>
+        <Route path='/' element={<Layout/>} errorElement={<ErrorPage/>}>
           <Route index={true} element={<All/>}/>
           <Route path='active' element={<Active/>}/>
           <Route path='completed' element={<Completed/>}/>
         </Route>
-      </Route>
     </>
   )
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
   </React.StrictMode>,
 )
